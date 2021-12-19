fb6c343a1591b8e4562eb2e3d3c4df40a359098c
GIT-H4R5X <95856624+GIT-H4R5X@users.noreply.github.com>
2021-12-19 10:48:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,10 +23,10 @@ BOT_USERNAME = os.environ.get("BOT_USERNAME") # Your Bot's Username without "@"
 DATABASE_URL = os.environ.get("DATABASE_URL") #mongo database url for more info contact in support group
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # Your Log Channel! Make a private channel and get it's ID
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # If you need to broadcast messages as a copy or Forwarded Message
-THUMB_URL = os.environ.get("THUMB_URL", "https://telegra.ph/file/e10da38412ca7fe5bce30.jpg")
-XMARTY_QUE = os.environ.get("XMARTY_QUE", "https://telegra.ph/file/0ae55070cf0c4413817f8.jpg")
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "XMARTY_UPDATES")
-XMARTY_SUPPORT = os.environ.get("XMARTY_SUPPORT", "XMARTY_SUPPORT")
+THUMB_URL = os.environ.get("THUMB_URL", "https://telegra.ph/file/b7ad210015a8223e93909.jpg")
+XMARTY_QUE = os.environ.get("XMARTY_QUE", "https://telegra.ph/file/b7ad210015a8223e93909.jpg")
+UPDATES_CHANNEL = os.environ.get("CHANNEL", "RITISHAWORLDSSR)
+XMARTY_SUPPORT = os.environ.get("SUPPORT", "RITISHAWORLDSSR)
 
 # SOON ADDING 
 ARQ_API_KEY = getenv("ARQ_API_KEY")
@@ -36,7 +36,7 @@ ARQ_API_URL = "https://thearq.tech/"
 # Updator Configs
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/S780821/ELITE_MUSIC_BOT")
+UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/GIT-H4R5X/HARSHxRITISHAVCPLAYER")
 U_BRANCH = "xmarty"
 HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
 
